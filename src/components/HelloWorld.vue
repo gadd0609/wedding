66ca617dbf7647cd935f085cb9d55c95ced9c65d
gadd0609 <alegaddiel@gmail.com>
2024-11-15 01:26:20 -0600

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="relative max-w-xs border border-solid border-gray-200 rounded-2xl p-4 transition-all duration-500 col-span-12  xl:p-7   lg:col-span-3 md:col-span-6 ">
+  <div class=" mb-6 ">
+<svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M8.66699 12.8162L11.3501 15.4993C11.5616 15.7107 11.9043 15.7109 12.1158 15.4997L17.8753 9.75033M13.0003 23.8337C7.01724 23.8337 2.16699 18.9834 2.16699 13.0003C2.16699 7.01724 7.01724 2.16699 13.0003 2.16699C18.9834 2.16699 23.8337 7.01724 23.8337 13.0003C23.8337 18.9834 18.9834 23.8337 13.0003 23.8337Z" stroke="#4F46E5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+</svg>
+</div>
+<h4 class="text-base font-semibold text-gray-900 mb-2 capitalize transition-all duration-500 ">Test boda</h4>
+<p class="text-sm font-normal text-gray-500 transition-all duration-500 leading-5 "> Apertura de repo </p>
+</div>
+
+
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
